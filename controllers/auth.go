63b3b99a3e2e493c8c0bf9e1101f1575d8e5cae3
Yuttasak(Yo)Pannawat <yuttasak.pan.true@gmail.com>
2023-07-10 19:57:17 +0700

    feat(Improved): :art: [Create] Models user

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,7 +1,14 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	models "github.com/yuttasakcom/go-fiber-simple/medels"
+)
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, Fiber👋")
+func Register(c *fiber.Ctx) error {
+	user := new(models.User)
+	user.FirstName = "Yuttasak"
+	user.LastName = "Pannawat"
+	user.Email = "yuttasakcom@gmail.com"
+	return c.JSON(user)
 }
---
+++ medels/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID       uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Email string `json:"email"`
+	Password string `json:"password,omitempty"`
+}
